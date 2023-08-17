3f00ad4798406ac3a079212323ed271309f0713d
Hector Sosa <hello@hectorsosa.me>
2023-08-18 01:20:57 +0200

    Make up for construction pages

--- src/components/preview/Homepage.tsx
+++ src/components/preview/Homepage.tsx
@@ -1,22 +1,20 @@
-import Link from "next/link";
+import { CTA } from "@components/ui/CTA";
 
 export const Homepage = () => {
   return (
     <div className="mx-auto grid place-content-center place-items-center h-full w-full gap-6 text-center">
       <section className="space-y-6">
         <h1 className="text-4xl font-semibold md:text-6xl">
-          Under Construction
+          Welcome to Preview
         </h1>
         <p className="text-lg leading-relaxed text-neutral-500">
           This page is still under construction! Thanks for your patience!
         </p>
-        <div className="w-full md:flex md:gap-4 md:justify-center">
-          <Link
-            href={"/docs"}
-            className="border block w-full md:w-fit px-8 py-4 rounded-xl bg-white text-black font-medium"
-          >
-            Go back to Documentation
-          </Link>
+        <div className="lg:space-x-2 space-y-2">
+          <CTA href="/docs">Documentation</CTA>
+          <CTA secondary href="/templates">
+            Templates
+          </CTA>
         </div>
       </section>
     </div>
